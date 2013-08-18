62ce8eccc75b8e806e2437ccc6d00fbd7ece28d5
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-08-18 16:30:09 -0700

    cross-compile support in `yard build`

--- scripts/_init.sh
+++ scripts/_init.sh
@@ -3,3 +3,91 @@
 function error() {
     echo "$@" 1>&2
 }
+
+# prints space separated os and arch for build targets. one or both may be
+# empty strings, in which case `go build` will figure it out.
+function go_os_arch() {
+    TARGET_OS=$1
+    TARGET_ARCH=$2
+
+    if [[ -z "$TARGET_OS" ]]; then
+        TARGET_OS="$GOOS"
+    fi
+
+    # target os must be unixy.
+    case "$TARGET_OS" in
+        darwin|freebsd|netbsd|openbsd|linux)
+            # ok
+            ;;
+        "")
+            # FIXME attempt to uname it out
+            ;;
+        plan9)
+            echo 1>&2 'plan9 is not supported (syslog) *sadface*'
+            return 1
+            ;;
+        windows)
+            echo 1>&2 'windows is not supported'
+            return 1
+            ;;
+        *)
+            echo 1>&2 "target os not supported: $TARGET_OS"
+            return 1
+            ;;
+    esac
+
+    if [[ -z "$TARGET_ARCH" ]]; then
+        TARGET_ARCH="$GOARCH"
+    fi
+
+    case "$TARGET_ARCH" in
+        arm|386|amd64)
+            # ok
+            ;;
+        "")
+            # FIXME attempt to uname it out
+            ;;
+        *)
+            echo 1>&2 "target arch not supported: $TARGET_ARCH"
+            return 1
+    esac
+
+    if [[ -n "$TARGET_OS" ]] && [[ -n "$TARGET_ARCH" ]]; then
+        case "$TARGET_OS $TARGET_ARCH" in
+            "darwin 386")
+                ;;
+            "darwin amd64")
+                ;;
+            "freebsd 386")
+                ;;
+            "freebsd amd64")
+                ;;
+            "linux 386")
+                ;;
+            "linux amd64")
+                ;;
+            "linux arm")
+                ;;
+            "netbsd 386")
+                ;;
+            "netbsd amd64")
+                ;;
+            "openbsd 386")
+                ;;
+            "openbsd amd64")
+                ;;
+            "plan9 386")
+                ;;
+            "windows 386")
+                ;;
+            "windows amd64")
+                ;;
+            *)
+                echo 1>&2 "invalid os-arch combination: $TARGET_OS $TARGET_ARCH"
+                return 1
+        esac
+    fi
+
+    echo "$TARGET_OS $TARGET_ARCH"
+    return 0
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 function build() {
+    TARGET_OS_ARCH=$(go_os_arch $1 $2)
+    TARGET_OS_ARCH_ERR=$?
+    if [ $TARGET_OS_ARCH_ERR -ne 0 ]; then
+        echo "$TARGET_OS_ARCH"
+        return 1
+    fi
+
+    export GOOS=`echo "$TARGET_OS_ARCH" | awk '{print $1}'`
+    export GOARCH=`echo "$TARGET_OS_ARCH" | awk '{print $2}'`
+
     go get -v ./src/cmd/blockd
     go build -v -o bin/blockd ./src/cmd/blockd
 }
